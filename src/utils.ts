a6a28f3b80461ab5a6a64f9cdf55d080f50eb843
Juphet Vitualla <jvitualla@dnamicro.com>
2024-05-16 04:10:53 +0800

    update link

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,5 @@
 import path from "path";
-export const BASE_URL = "https://dna-contract-development.vercel.app/";  
+export const BASE_URL = "http://form-contract.development.local/";  
 
 export const public_path = path.join(process.cwd(), 'public');
 
